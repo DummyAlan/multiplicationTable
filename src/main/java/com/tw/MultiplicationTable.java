c09ec1252e54e9a0915f67d951634336ced1c5f8
Alan Wan <alan.wan@oocl.com>
2022-11-29 15:38:40 +0800

    fea: passed all testcases

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,30 +2,48 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int start, int end) {
+        if(isValid(start, end)){
+            return generateTable(start, end);
+        }
         return null;
     }
 
     public Boolean isValid(int start, int end) {
-        return null;
+        return isStartNotBiggerThanEnd(start, end) && isInRange(start) && isInRange(end);
     }
 
     public Boolean isInRange(int number) {
-        return null;
+        return number > 0 && number < 1000;
     }
 
     public Boolean isStartNotBiggerThanEnd(int start, int end) {
-        return null;
+        return start <= end;
     }
 
     public String generateTable(int start, int end) {
-        return null;
+        String result = "";
+        for(int i = start; i <= end ;i++){
+            if (i == end){
+                result += generateLine(start, i);
+            }
+            else {
+                result += generateLine(start, i) + System.lineSeparator();
+            }
+        }
+
+        return result;
     }
 
     public String generateLine(int start, int row) {
-        return null;
+        String line = "";
+        for (int i = start; i <= row; i++){
+            line += generateSingleExpression(i, row)+"  ";
+        }
+        return line.substring(0,line.length()-2);
     }
 
     public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
+        String temp = multiplicand+"*"+multiplier+"="+(multiplicand*multiplier);
+        return temp;
     }
 }
